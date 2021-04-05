945800dcb355ff1e1ffb644f9a817eb696748d14
Han not working these days <wooyeophan@gmail.com>
2021-04-05 11:10:00 +0900

    수열 합 함수들

--- MathCodingAgain/Math.cpp
+++ MathCodingAgain/Math.cpp
@@ -1,18 +1,64 @@
 #include <iostream>
 #include <cmath>
 
-int Isometric(int tolerance, int begin, int beginNum, int end)
-{
-	return beginNum + tolerance * (end - begin);
-}
-long long Geometric(int common, int begin, int beginNum, int end)
-{
-	return beginNum * pow(common, end - begin);
-}
+#pragma region 등차 등비
+int isometric(int tolerance, int begin, int beginNum, int end);
+long long geometric(int common, int begin, int beginNum, int end);
+#pragma endregion
+
+#pragma region 수열 합
+int mirrorMetric(int begin, int end);
+int mirrorOddMetric(int begin, int end);
+long powint(int end);
+long pow3int(int end);
+#pragma endregion
+
 
 int main()
 {
-	std::cout << Isometric(5, 1, 1, 400) << std::endl;
-	std::cout << Geometric(5, 1, 1, 10) << std::endl;
+#pragma region 등차 등비
+	//std::cout << isometric(5, 1, 1, 400) << std::endl;
+	//std::cout << geometric(5, 1, 1, 10) << std::endl;
+#pragma endregion
+
+#pragma region 수열 합
+	std::cout << mirrorMetric(1, 7) << std::endl;
+	std::cout << mirrorOddMetric(1, 9) << std::endl;
+	std::cout << powint(4) << std::endl;
+	std::cout << pow3int(4) << std::endl;
+#pragma endregion
+
 	return(0);
 }
+#pragma region 등차 등비
+int isometric(int tolerance, int begin, int beginNum, int end)
+{
+	return beginNum + tolerance * (end - begin);
+}
+
+long long geometric(int common, int begin, int beginNum, int end)
+{
+	return beginNum * pow(common, end - begin);
+}
+#pragma endregion
+
+#pragma region 수열 합
+int mirrorMetric(int begin, int end)
+{
+	return end * end;
+}
+int mirrorOddMetric(int begin, int end)
+{
+	return (end / 2) * (end / 2) +
+		((end + 1) / 2) * ((end + 1) / 2);
+}
+long powint(int end)
+{
+	return (end * (end + 1) * ((2 * end) + 1)) / 6;
+}
+long pow3int(int end)
+{
+	return ((end * (end + 1)) / 2) * 
+		((end * (end + 1)) / 2);
+}
+#pragma endregion
